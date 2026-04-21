d77171b9b38e5ea5346cea1babbdcf37d88b9969
AndryMorgan <rafanoharanamorgan4@gmail.com>
2026-04-21 15:30:51 +0300

    Implementation

---
+++ src/test/java/com/collectivity/agricultural/AgriculturalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectivity.agricultural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
