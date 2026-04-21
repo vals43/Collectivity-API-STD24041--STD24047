e489784b096a48592278991438dde15c3426609f
Vals43 <hei.teddy.3@gmail.com>
2026-04-21 16:39:57 +0300

    fix: db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,6 @@
 -- 1. Définition des Types Énumérés (Propre à Postgres)
 CREATE TYPE gender_type AS ENUM ('MALE', 'FEMALE');
 CREATE TYPE occupation_type AS ENUM ('JUNIOR', 'SENIOR', 'SECRETARY', 'TREASURER', 'VICE_PRESIDENT', 'PRESIDENT');
-CREATE TYPE payment_type_enum AS ENUM ('REGISTRATION', 'ANNUAL_DUES');
 
 -- 2. Table des COLLECTIVITÉS
 CREATE TABLE collectivities (
@@ -42,26 +41,4 @@ CREATE TABLE member_referees (
                                  PRIMARY KEY (candidate_id, referee_id),
                                  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES members(id) ON DELETE CASCADE,
                                  CONSTRAINT fk_referee FOREIGN KEY (referee_id) REFERENCES members(id) ON DELETE CASCADE
-);
-
--- 5. Table des MANDATS (Historique pour la règle des 2 mandats max)
-CREATE TABLE mandates (
-                          id SERIAL PRIMARY KEY,
-                          year INT NOT NULL,
-                          member_id VARCHAR(50) NOT NULL,
-                          collectivity_id VARCHAR(50), -- NULL pour les mandats nationaux
-                          role occupation_type NOT NULL,
-                          is_federation_level BOOLEAN DEFAULT FALSE,
-                          CONSTRAINT fk_member_mandate FOREIGN KEY (member_id) REFERENCES members(id),
-                          CONSTRAINT fk_coll_mandate FOREIGN KEY (collectivity_id) REFERENCES collectivities(id)
-);
-
--- 6. Table des PAIEMENTS
-CREATE TABLE payments (
-                          id SERIAL PRIMARY KEY,
-                          member_id VARCHAR(50) NOT NULL,
-                          amount NUMERIC(12, 2) NOT NULL, -- NUMERIC est plus précis que DECIMAL pour l'Ariary
-                          p_type payment_type_enum NOT NULL,
-                          payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          CONSTRAINT fk_member_payment FOREIGN KEY (member_id) REFERENCES members(id)
 );
