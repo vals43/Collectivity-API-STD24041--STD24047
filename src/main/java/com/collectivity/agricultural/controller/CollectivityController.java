ca853794f3fe1e85936aa1668afcd938648e8fbc
Vals43 <hei.teddy.3@gmail.com>
2026-04-23 10:06:19 +0300

    feat: OAS 0.0.4

--- src/main/java/com/collectivity/agricultural/controller/CollectivityController.java
+++ src/main/java/com/collectivity/agricultural/controller/CollectivityController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/collectivities")
-public class CollectivityController {
+public class   CollectivityController {
     private final CollectivityService service;
 
     @PostMapping
